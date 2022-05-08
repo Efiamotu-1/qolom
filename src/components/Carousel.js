61d20e313b30ccbbe0fccebd0b638c4379d1d0f3
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-05-08 10:07:32 +0100

    new changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -28,7 +28,7 @@ function Carousels() {
 
   return (
     <Carousel fade variant="dark" controls={false} indicators={false}>
-    <Carousel.Item interval={1000}>
+    <Carousel.Item interval={1500}>
       <div className={classes.opacity}>
 <img
         className="d-block w-100"
@@ -43,7 +43,7 @@ function Carousels() {
         <p>Be present only when you need to.</p>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={1000}> 
+    <Carousel.Item interval={1500}> 
       <img
         className="d-block w-100"
         src={covidQueue}
@@ -55,7 +55,7 @@ function Carousels() {
         <p>Make your own line and manage the queue.</p>
       </Carousel.Caption>
     </Carousel.Item>
-    <Carousel.Item interval={1000}> 
+    <Carousel.Item interval={1500}> 
       <img
         className="d-block w-100"
         src={covidQueue}
