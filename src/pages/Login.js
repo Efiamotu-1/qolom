d07098a142653d5ee0593ff550ce0f8ebbccdbcc
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-06-08 17:32:33 +0100

    new changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -31,7 +31,7 @@ function Login() {
   
     if (email.includes('@') && password.trim().length > 7) {
       
-      fetch(' http://localhost:4000/users', {
+      fetch('http://54.227.124.36:9000/api/account/business-profile/sign-in/', {
         method : 'POST',
         headers : {"content-type" : "application/json"},
         body : JSON.stringify({email, password})
